08c70c522abcaf2db406c1160cd30f5c9293e0d8
Vamsi Krishna Myneni <32959424+VamsiKrishnaMyneni@users.noreply.github.com>
2025-02-03 21:37:09 +0530

    table style changes

--- src/products/Products.tsx
+++ src/products/Products.tsx
@@ -18,42 +18,43 @@ function Products() {
             {data && data.length > 0 && (
                 <div className='products-container'>
                     <h2>Products Comparison</h2>
-                    <table className="products-table">
-                        <thead>
-                            <tr>
-                                <th></th>
-                                <th>Image</th>
-                                <th>Title</th>
-                                <th>Price</th>
-                                <th>Rating</th>
-                                <th>Description</th>
-                                <th>Category</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {data.map((product) => (
-                                <tr key={product.id}>
-                                    <td><input type="checkbox" className='product-select' onChange={() => onProductChange(product.id)} /></td>
-                                    <td>  <img src={product.image} alt={product.title} className="product-image" /></td>
-                                    <td>
-                                        <div className="product-title" title={product.title}>{product.title}</div>
-                                    </td>
-                                    <td>{product.price}</td>
-
-                                    <td><div className="star-rating" style={{ "--rating": `${product.rating.rate}` } as React.CSSProperties} aria-label="Rating of this product is 2.3 out of 5."></div>
-                                        <div className='ratings-count'>{product.rating.count} ratings</div>  </td>
-                                    <td> <div className="product-description" title={product.description}>
-                                        {product.description.length > 100 ? `${product.description.slice(0, 100)}...` : product.description} </div>
-                                    </td>
-                                    <td>{product.category}</td>
+                    <div className='products-table-container'>
+                        <table className="products-table">
+                            <thead>
+                                <tr>
+                                    <th></th>
+                                    <th>Image</th>
+                                    <th>Title</th>
+                                    <th>Price</th>
+                                    <th>Rating</th>
+                                    <th>Description</th>
+                                    <th>Category</th>
                                 </tr>
-                            ))}
-                        </tbody>
-                    </table>
+                            </thead>
+                            <tbody>
+                                {data.map((product) => (
+                                    <tr key={product.id}>
+                                        <td className='prodcut-select-cell'><input type="checkbox" className='product-select' onChange={() => onProductChange(product.id)} /></td>
+                                        <td>  <img src={product.image} alt={product.title} className="product-image" /></td>
+                                        <td>
+                                            <div className="product-title" title={product.title}>{product.title}</div>
+                                        </td>
+                                        <td>{product.price}</td>
+
+                                        <td><div className="star-rating" style={{ "--rating": `${product.rating.rate}` } as React.CSSProperties} aria-label="Rating of this product is 2.3 out of 5."></div>
+                                            <div className='ratings-count'>{product.rating.count} ratings</div>  </td>
+                                        <td> <div className="product-description" title={product.description}>
+                                            {product.description.length > 100 ? `${product.description.slice(0, 100)}...` : product.description} </div>
+                                        </td>
+                                        <td>{product.category}</td>
+                                    </tr>
+                                ))}
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
             )}
         </>
     )
 }
-
 export default Products;
