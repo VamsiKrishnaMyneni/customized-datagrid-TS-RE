b725452fded3b7d8cab8d02a77dedcc4a3427534
Vamsi Krishna Myneni <32959424+VamsiKrishnaMyneni@users.noreply.github.com>
2025-03-30 16:54:02 +0530

    products set up

---
+++ src/products/Products.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+interface Props { }
+
+function Products(props: Props) {
+    const { } = props
+
+    return (
+        <>
+        </>
+    )
+}
+
+export default Products;
