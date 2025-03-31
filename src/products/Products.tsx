472e59afdd3c6984159187f576858d1352e0570d
Vamsi Krishna Myneni <32959424+VamsiKrishnaMyneni@users.noreply.github.com>
2025-03-31 16:04:48 +0530

    table style changes

--- src/products/Products.tsx
+++ src/products/Products.tsx
@@ -22,26 +22,30 @@ function Products() {
                         <thead>
                             <tr>
                                 <th></th>
+                                <th>Image</th>
                                 <th>Title</th>
                                 <th>Price</th>
                                 <th>Rating</th>
                                 <th>Description</th>
-                                <th></th>
+                                <th>Category</th>
                             </tr>
                         </thead>
                         <tbody>
                             {data.map((product) => (
                                 <tr key={product.id}>
                                     <td><input type="checkbox" onChange={() => onProductChange(product.id)} /></td>
+                                    <td>  <img src={product.image} alt={product.title} className="product-image" /></td>
                                     <td>
-                                        <div className="product-title-container">
-                                            <img src={product.image} alt={product.title} className="product-image" />
-                                            <span className="product-title" title={product.title}>{product.title}</span>
-                                        </div>
+                                        <div className="product-title" title={product.title}>{product.title}</div>
                                     </td>
                                     <td>{product.price}</td>
-                                    <td><div className="star-rating" style={{ "--rating": `${product.rating.rate}` } as React.CSSProperties} aria-label="Rating of this product is 2.3 out of 5."></div></td>
-                                    <td>{product.description}</td>
+
+                                    <td><div className="star-rating" style={{ "--rating": `${product.rating.rate}` } as React.CSSProperties} aria-label="Rating of this product is 2.3 out of 5."></div>
+                                        <div className='ratings-count'>{product.rating.count} ratings</div>  </td>
+                                    <td> <div className="product-description" title={product.description}>
+                                        {product.description.length > 100 ? `${product.description.slice(0, 100)}...` : product.description} </div>
+                                    </td>
+                                    <td>{product.category}</td>
                                 </tr>
                             ))}
                         </tbody>
